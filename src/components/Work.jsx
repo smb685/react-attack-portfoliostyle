743638fa53db22e823aa08c5a40d0c5180f64000
Sylvester Brandon <sylvester.brandon25@gmail.com>
2022-09-07 20:43:52 -0500

    .

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import WorkImg from '../assets/working.jpeg';
 import passwordGen from '../assets/passwordgen.jpeg';
 import horiseonSo from '../assets/horiseon.jpeg';
+import addPort from '../assets/addPort.png'
 
 const Work = () => {
   return (
@@ -81,6 +82,29 @@ const Work = () => {
                   </button>
                 </a>
                 <a href='https://github.com/seanxmcdaniel/events-blogd'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${addPort})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
+            {/* Hover Effects */}
+            <div className='opacity-0 group-hover:opacity-100'>
+              <span className='text-2xl font-bold text-white tracking-wider'>
+                Mern Application Team Project
+              </span>
+              <div className='pt-8 text-center'>
+                <a href='https://thawing-brook-13054.herokuapp.com/'>
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Demo
+                  </button>
+                </a>
+                <a href='https://github.com/mleone13/project-3'>
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
